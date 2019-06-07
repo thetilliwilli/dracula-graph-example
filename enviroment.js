aa42b38824a1a174042dffd7612ca1330df19604
TilliWilli <tilliwilli@gmail.com>
2019-06-07 20:42:45 +0300

    init

---
+++ enviroment.js
@@ -0,0 +1,424 @@
+window.w = {
+    "general": {
+        "renderTo": "widget",
+        "type": "UserWidget"
+    },
+    "colors": [
+        "rgba(40, 238, 150, 1)",
+        "rgba(255, 217, 0, 1)",
+        "rgba(255, 70, 69, 1)",
+        "rgba(181, 181, 181, 1)",
+        "rgba(60, 146, 255, 1)",
+        "rgba(100, 229, 114, 1)",
+        "rgba(255, 150, 85, 1)",
+        "rgba(255, 242, 99, 1)",
+        "rgba(106, 249, 196, 1)",
+        "rgba(145, 232, 225, 1)"
+    ],
+    "data": {
+        "$type": "Visiology.VA.BL.DataFrame.Entities.DataFrame, Visiology.VA.BL.DataFrame",
+        "cols": [
+            [
+                "WeightOfEdge",
+                "Label_1",
+                "3",
+                "90855463",
+                "2"
+            ],
+            [
+                "WeightOfEdge",
+                "Label_13",
+                "10",
+                "69459125",
+                "3"
+            ],
+            [
+                "WeightOfEdge",
+                "Label_2",
+                "9",
+                "72144177",
+                "3"
+            ],
+            [
+                "WeightOfEdge",
+                "Label_3",
+                "14",
+                "29336977",
+                "3"
+            ],
+            [
+                "WeightOfEdge",
+                "Label_4",
+                "11",
+                "68268286",
+                "3"
+            ],
+            [
+                "WeightOfEdge",
+                "Label_5",
+                "12",
+                "39097258",
+                "3"
+            ]
+        ],
+        "rows": [
+            [
+                "Label_1",
+                "3",
+                "90855463",
+                "2"
+            ],
+            [
+                "Label_10",
+                "7",
+                "73424428",
+                "3"
+            ],
+            [
+                "Label_11",
+                "13",
+                "35306958",
+                "3"
+            ],
+            [
+                "Label_12",
+                "4",
+                "87472173",
+                "2"
+            ],
+            [
+                "Label_13",
+                "10",
+                "69459125",
+                "3"
+            ],
+            [
+                "Label_14",
+                "2",
+                "94047505",
+                "1"
+            ],
+            [
+                "Label_2",
+                "9",
+                "72144177",
+                "3"
+            ],
+            [
+                "Label_3",
+                "14",
+                "29336977",
+                "3"
+            ],
+            [
+                "Label_4",
+                "11",
+                "68268286",
+                "3"
+            ],
+            [
+                "Label_5",
+                "12",
+                "39097258",
+                "3"
+            ],
+            [
+                "Label_6",
+                "1",
+                "94832224",
+                "1"
+            ],
+            [
+                "Label_7",
+                "5",
+                "78012911",
+                "2"
+            ],
+            [
+                "Label_8",
+                "8",
+                "72226705",
+                "3"
+            ],
+            [
+                "Label_9",
+                "6",
+                "75597868",
+                "2"
+            ]
+        ],
+        "values": [
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                100358799,
+                null,
+                null,
+                676859025,
+                126316556,
+                697098709,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                1086267497,
+                null,
+                null,
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                1285816902,
+                null,
+                null,
+                null,
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                170361937
+            ],
+            [
+                null,
+                654625310,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            ],
+            [
+                null,
+                null,
+                251423459,
+                1432124205,
+                953124696,
+                266486546,
+                988217868,
+                759464041,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+            ]
+        ],
+        "colsMeta": [
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_1",
+                    "3",
+                    "90855463",
+                    "2"
+                ]
+            },
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_13",
+                    "10",
+                    "69459125",
+                    "3"
+                ]
+            },
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_2",
+                    "9",
+                    "72144177",
+                    "3"
+                ]
+            },
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_3",
+                    "14",
+                    "29336977",
+                    "3"
+                ]
+            },
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_4",
+                    "11",
+                    "68268286",
+                    "3"
+                ]
+            },
+            {
+                "measureGroupId": [
+                    "GraphDraakula_GP"
+                ],
+                "measureId": [
+                    "WeightOfEdge"
+                ],
+                "dimensions": [
+                    "Label_5",
+                    "12",
+                    "39097258",
+                    "3"
+                ]
+            }
+        ],
+        "colsInfo": [
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_1\",\"3\",\"90855463\",\"2\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_1",
+                    "3",
+                    "90855463",
+                    "2"
+                ]
+            },
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_13\",\"10\",\"69459125\",\"3\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_13",
+                    "10",
+                    "69459125",
+                    "3"
+                ]
+            },
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_2\",\"9\",\"72144177\",\"3\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_2",
+                    "9",
+                    "72144177",
+                    "3"
+                ]
+            },
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_3\",\"14\",\"29336977\",\"3\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_3",
+                    "14",
+                    "29336977",
+                    "3"
+                ]
+            },
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_4\",\"11\",\"68268286\",\"3\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_4",
+                    "11",
+                    "68268286",
+                    "3"
+                ]
+            },
+            {
+                "$type": "Visiology.VA.BL.DataFrame.Entities.ColInfo, Visiology.VA.BL.DataFrame",
+                "id": "\"GraphDraakula_GP\",\"WeightOfEdge\",\"Label_5\",\"12\",\"39097258\",\"3\"",
+                "idParts": [
+                    "GraphDraakula_GP",
+                    "WeightOfEdge",
+                    "Label_5",
+                    "12",
+                    "39097258",
+                    "3"
+                ]
+            }
+        ]
+    }
+};
+
+window.visApi = function(){
+    return {
+        getWidgetByGuid(widgetId){
+            return {
+                widgetState: {
+                    propertiesJson : JSON.stringify({ lineWidth: 6 })
+                }
+            }
+        }
+    }
+}
+
+
+class TextRender { }
---
+++ main.js
@@ -0,0 +1,117 @@
+//
+//два файла содержащие саму библиотеку (01_rephael.min.js, 02_dracula.min.js) необходимо положить в VIP-Dashboards\Sources\PL\Viewer\Visiology.VA.PL.DashboardViewer\wwwroot\custom
+//
+
+console.log(w);
+
+var widgetId = w.general.renderTo;
+var widgetAnchor = "#" + widgetId;
+var $widget = $(widgetAnchor);
+var globalColors = w.colors;
+var propertiesJson = JSON.parse(visApi().getWidgetByGuid(widgetId).widgetState.propertiesJson);
+var globalProperties = {
+    lineWidth: propertiesJson.lineWidth || 6,
+};
+
+function convertToGraph(data, vertices, adjacencyList) {
+
+    var sourceVerticesInfo = data.cols
+        .map(x => x.slice(1)) //удаляем строку с названием показателя
+        .map(vertexInfo => ({ label: vertexInfo[0], weight: Number(vertexInfo[1]), vertexId: vertexInfo[2], type: vertexInfo[3] }))
+        ;
+
+    var targetVerticesInfo = data.rows
+        .map(vertexInfo => ({ label: vertexInfo[0], weight: Number(vertexInfo[1]), vertexId: vertexInfo[2], type: vertexInfo[3] }))
+        ;
+
+    vertices.push(...sourceVerticesInfo, ...targetVerticesInfo);
+    vertices.minWeight = Math.min(...vertices.map(x => x.weight));
+    vertices.maxWeight = Math.max(...vertices.map(x => x.weight));
+    vertices.count = vertices.length;
+    vertices.calcRelativeWeight = (weight) => (weight - vertices.minWeight) / (vertices.maxWeight - vertices.minWeight);
+
+    adjacencyList.minWeight = Number.MAX_VALUE;
+    adjacencyList.maxWeight = 0;
+    adjacencyList.count = 0;
+    adjacencyList.calcRelativeWeight = (weight) => (weight - adjacencyList.minWeight) / (adjacencyList.maxWeight - adjacencyList.minWeight);
+
+    data.values.forEach((row, rowIndex) => row.forEach((edgeWeight, columnIndex) => {
+
+        var sourceVertex = sourceVerticesInfo[rowIndex];
+        var targetVertex = targetVerticesInfo[columnIndex];
+
+        if (edgeWeight != null) {
+            adjacencyList.push({ sourceVertex, targetVertex, edgeWeight });
+            adjacencyList.minWeight = Math.min(adjacencyList.minWeight, edgeWeight);
+            adjacencyList.maxWeight = Math.max(adjacencyList.maxWeight, edgeWeight);
+            adjacencyList.count++;
+        }
+
+    }));
+
+}
+
+var vertices = [];
+var adjacencyList = [];
+convertToGraph(w.data, vertices, adjacencyList);
+
+
+function VertexRenderer(r, n) {
+    var safeWeight = 1 + n.weight; // 1 - 1.5
+    const parsedColor = Raphael.color(globalColors[n.type % globalColors.length]);
+    var color = Raphael.rgb(parsedColor.r, parsedColor.g, parsedColor.b);
+
+    var set = r.set();
+
+    var ellipse = r.ellipse(0, 0, 30 * safeWeight, 20 * safeWeight)
+        .attr({ fill: color, 'stroke-width': 2, 'fill-opacity': 1 });
+
+    var text = r.text(0, 0, n.label)
+        .attr({ 'font-size': `${12 * safeWeight}px`, "user-select": "none" });
+
+    set
+        .push(text)
+        .push(ellipse)
+        ;
+
+    return set;
+}
+
+function drawGraph(vertices, adjacencyList) {
+
+    var widgetWidth = $widget.width();
+    var widgetHeight = $widget.height();
+
+    $(widgetAnchor).empty();
+
+    var g = new Dracula.Graph();
+
+    vertices.map(vertex => g.addNode(vertex.vertexId, { label: vertex.label, render: VertexRenderer, weight: vertices.calcRelativeWeight(vertex.weight), type: vertex.type }));
+
+    adjacencyList.map(edge => g.addEdge(edge.sourceVertex.vertexId, edge.targetVertex.vertexId, { stroke: "darkgrey", weight: adjacencyList.calcRelativeWeight(edge.edgeWeight) }));
+
+    var layouter = new Dracula.Layout.Spring(g);
+    layouter.layout();
+
+    var draculaRenderer = new Dracula.Renderer.Raphael(widgetAnchor, g, widgetWidth, widgetHeight);
+    draculaRenderer.draw();
+    redrawEdgeThinkness(g);
+    console.log(g);
+}
+
+function redrawEdgeThinkness(graph) {
+    graph.edges.forEach(edge => {
+        edge.shape.fg.attr({ "stroke-width": 1 + globalProperties.lineWidth * edge.style.weight });
+        // edge.shape.label.attr("y", edge.shape.label.attr("y") + 13);
+    });
+}
+
+drawGraph(vertices, adjacencyList);
+
+//!!!должно быть последней строчкой!!! - возвращаем инстанс класса WidgetRenderer
+({
+    //hack для получения ссылки на конструктор класса WidgetRenderer
+    //содержит конструктор класса WidgetRenderer
+    __proto__: (new TextRender({ general: { renderTo: "" }, text: { text: "" }, style: {} })).__proto__,
+    updateDimensions: () => drawGraph(vertices, adjacencyList),
+})
